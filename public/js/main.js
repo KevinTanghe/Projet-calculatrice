7752d9ed8f6e30844c71497272d9d32dab2d8395
Kevin <tanghekevin1@gmail.com>
2020-12-16 10:23:12 +0100

    structure plus style fais, commence le js

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+let div = document.querySelectorAll(".containerChiffre");
+let input = document.querySelector("input")
+let buttonCol1 = div[0].querySelectorAll("button");
+let buttonCol2 = div[1].querySelectorAll("button");
+let buttonCol3 = div[2].querySelectorAll("button");
+let buttonCol4 = div[3].querySelectorAll("button");
+
+console.log(buttonCol1,buttonCol2,buttonCol3,buttonCol4);
